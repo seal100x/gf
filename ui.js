622e71e6fcec16650330dd3554659741c2fc0f1d
cty <seal100x@163.com>
2016-02-05 20:34:12 +0800

    no message

--- ui.js
+++ ui.js
@@ -4,18 +4,19 @@ function thead() {
 	$thead.append(td("ID", "ht_id"));
 	$thead.append(td("名称", "ht_name"));
 	$thead.append(td("类型", "ht_type"));
-	$thead.append(td("系统数据", "ht_abilityRank"));
+	$thead.append(td("星级", "ht_star"));
+	$thead.append(td("弹药/军粮", "ht_consume"));
 	$thead.append(td("血量", "ht_HP"));
-	$thead.append(td("弹药", "ht_ammunition"));
-	$thead.append(td("军粮", "ht_forage"));
 	$thead.append(td("伤害", "ht_damage"));
 	$thead.append(td("命中", "ht_hit"));
 	$thead.append(td("回避", "ht_dodge"));
-	$thead.append(td("移速", "ht_speed"));
 	$thead.append(td("射速", "ht_firerate"));
+	$thead.append(td("成长", "ht_abilityrank"));
+	$thead.append(td("移速", "ht_speed"));
 	$thead.append(td("建造时间", "ht_buildtime"));
-	$thead.append(td("技能效果", "ht_skilleffect"));
-	$thead.append(td("技能范围", "ht_skillrange"));
+	$thead.append(td("BUFF效果", "ht_buffeffect"));
+	$thead.append(td("BUFF范围", "ht_buffrange"));
+	$thead.append(td("技能效果", "ht_skill"));
 	
 	$td_nbsp = td("", "");
 	$td_nbsp = td("回到顶部", "th_gotop");
@@ -37,25 +38,29 @@ function row(data) {
 	$row.append(td(data.id, "td_id"));
 	$row.append(td(data.name, "td_name"));
 	$row.append(td(data.type, "td_type"));
-	$row.append(td(data.abilityRank.join(""), "td_abilityRank"));
-	$row.append(td(data.HP, "td_HP"));
-	$row.append(td(data.ammunition, "td_ammunition"));
-	$row.append(td(data.forage, "td_forage"));
-	$row.append(td(data.damage, "td_damage"));
-	$row.append(td(data.hit, "td_hit"));
-	$row.append(td(data.dodge, "td_dodge"));
+	$row.append(td(data.star, "td_star"));
+	$row.append(td(data.ammunition + "/" + data.forage, "td_consume"));
+	$row.append(td(data.HPRank + " | " + data.HP, "td_HP"));
+	$row.append(td(data.damageRank + " | " + data.damage, "td_damage"));
+	$row.append(td(data.hitRank + " | " + data.hit, "td_hit"));
+	$row.append(td(data.dodgeRank + " | " + data.dodge, "td_dodge"));
+	$row.append(td(data.firerateRank + " | " + data.firerate, "td_firerate"));
+	$row.append(td(data.abilityRank, "td_abilityrank"));
 	$row.append(td(data.speed, "td_speed"));
-	$row.append(td(data.firerate, "td_firerate"));
 	$row.append(td(data.buildtime, "td_buildtime"));
-	$row.append(td(data.skilleffect, "td_skilleffect"));
+	$row.append(td(data.buffeffect, "td_buffeffect"));
 	
-	var skillarea = "";
-	for(var i in data.skillrange){
-		skillarea += data.skillrange[i].replace("1","■").replace("0","□");
-		if(i%3 == 2)
-			skillarea+="<br>";
+	var buffarea = "";
+	if(data.buffarea){
+		var areas = data.buffarea.split("");
+		for(var i in areas){
+			buffarea += areas[i].replace("1","■").replace("0","□");
+			if(i%3 == 2)
+				buffarea+="<br>";
+		}		
 	}
-	$row.append(td(skillarea, "td_skillrange"));
+	$row.append(td(buffarea, "td_buffrange"));
+	$row.append(td(data.skill, "td_skill"));
 	$row.append(td(sciamachyButton(data.name, data.id), 'icon'));
 	return $row;
 }
